9d7a496cde025703094f2f62a9cea8f859c2ac71
maykonmichel <maykonmichelpalma@gmail.com>
2020-03-02 22:20:51 -0300

    Added mocked data

--- src/screens/TodoList/index.js
+++ src/screens/TodoList/index.js
@@ -1,13 +1,44 @@
-import React, {memo} from 'react';
-import {Image, View} from 'react-native';
+import React, {memo, useCallback} from 'react';
+import {FlatList, Image, View} from 'react-native';
 
 import styles from './styles';
 import logo from '../../assets/images/logo.png';
 
+import NewTodo from '../../components/NewTodo';
+import Todo from '../../components/Todo';
+
+const todoList = [
+  {
+    id: '1',
+    description: 'First todo',
+  },
+  {
+    id: '2',
+    description: 'Second todo',
+  },
+  {
+    id: '3',
+    description: 'Third one',
+  },
+];
+
 export default memo(() => {
+  const onDeleteTodo = useCallback(() => {}, []);
+
+  const onSave = useCallback(() => {}, []);
+
+  const renderItem = useCallback(
+    ({item}) => <Todo onDelete={onDeleteTodo} todo={item} />,
+    [onDeleteTodo],
+  );
+
   return (
     <View style={styles.container}>
       <Image source={logo} />
+      <View style={styles.list}>
+        <NewTodo onSave={onSave} />
+        <FlatList data={todoList} renderItem={renderItem} />
+      </View>
     </View>
   );
 });
--- src/screens/TodoList/styles.js
+++ src/screens/TodoList/styles.js
@@ -6,4 +6,11 @@ export default StyleSheet.create({
     alignItems: 'center',
     paddingTop: 30,
   },
+  list: {
+    flex: 1,
+    marginTop: 30,
+    padding: 15,
+    alignSelf: 'stretch',
+    backgroundColor: '#eef1f4',
+  },
 });
