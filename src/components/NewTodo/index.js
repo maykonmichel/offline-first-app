992b7dbee1422dd71cea242a169b3d8b5d592cfc
maykonmichel <maykonmichelpalma@gmail.com>
2020-03-02 21:57:54 -0300

    Added prop types and default props

--- src/components/NewTodo/index.js
+++ src/components/NewTodo/index.js
@@ -1,9 +1,10 @@
 import React, {memo, useCallback, useState} from 'react';
 import {Text, TextInput, TouchableOpacity, View} from 'react-native';
+import PropTypes from 'prop-types';
 
 import styles from './styles';
 
-export default memo(({onSave}) => {
+const NewTodo = memo(({onSave}) => {
   const [value, setValue] = useState('');
 
   const onPress = useCallback(() => onSave(value), [onSave, value]);
@@ -17,3 +18,13 @@ export default memo(({onSave}) => {
     </View>
   );
 });
+
+NewTodo.propTypes = {
+  onSave: PropTypes.func,
+};
+
+NewTodo.defaultProps = {
+  onSave: () => {},
+};
+
+export default NewTodo;
