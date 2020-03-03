97fd707256416e112cc952fb9dc36e83a05223fa
maykonmichel <maykonmichelpalma@gmail.com>
2020-03-02 21:47:06 -0300

    Styled todo component

--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -1,9 +1,11 @@
 import React, {memo} from 'react';
 import {Text, TouchableOpacity} from 'react-native';
 
+import styles from './styles';
+
 const Todo = memo(({todo: {description}}) => {
   return (
-    <TouchableOpacity>
+    <TouchableOpacity style={styles.container}>
       <Text>{description}</Text>
     </TouchableOpacity>
   );
