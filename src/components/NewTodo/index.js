782cbe09b602edaa3c5c7e32dcbea16003d34c80
maykonmichel <maykonmichelpalma@gmail.com>
2020-03-02 21:56:09 -0300

    Added on save function

--- src/components/NewTodo/index.js
+++ src/components/NewTodo/index.js
@@ -1,13 +1,17 @@
-import React, {memo} from 'react';
+import React, {memo, useCallback, useState} from 'react';
 import {Text, TextInput, TouchableOpacity, View} from 'react-native';
 
 import styles from './styles';
 
-export default memo(() => {
+export default memo(({onSave}) => {
+  const [value, setValue] = useState('');
+
+  const onPress = useCallback(() => onSave(value), [onSave, value]);
+
   return (
     <View style={styles.container}>
-      <TextInput style={styles.input} />
-      <TouchableOpacity style={styles.button}>
+      <TextInput style={styles.input} value={value} onChangeText={setValue} />
+      <TouchableOpacity style={styles.button} onPress={onPress}>
         <Text style={styles.text}>SALVAR</Text>
       </TouchableOpacity>
     </View>
