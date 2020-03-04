88919dc6644df12c3f101709ab86bd8437b7fb3b
maykonmichel <maykonmichelpalma@gmail.com>
2020-03-04 08:23:57 -0300

    Added export cache

--- src/store/api.js
+++ src/store/api.js
@@ -2,7 +2,9 @@ import {ApolloClient} from 'apollo-client';
 import {HttpLink} from 'apollo-link-http';
 import {InMemoryCache} from 'apollo-cache-inmemory';
 
+export const cache = new InMemoryCache();
+
 export default new ApolloClient({
-  link: new HttpLink({uri: 'http://localhost:4000'}),
-  cache: new InMemoryCache(),
+  cache,
+  link: new HttpLink({uri: 'http://10.0.2.2:4000'}),
 });
